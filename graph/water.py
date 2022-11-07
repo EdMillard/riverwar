1bdde6aa370c00866f23dd8648509f504bb5190f
Ed Millard <ed.millard@gmail.com>
2022-11-07 09:54:16 -0700

    Template for adding table support

--- graph/water.py
+++ graph/water.py
@@ -30,6 +30,7 @@ from matplotlib.dates import YearLocator
 # gridspec to resize subplots
 # https://www.geeksforgeeks.org/how-to-create-different-subplot-sizes-in-matplotlib/
 
+
 class WaterGraph(object):
     """
     Essential paramaters
@@ -482,3 +483,25 @@ def positive_values(a):
         idx += 1
 
     return b
+
+
+def table():
+    val1 = ["{:X}".format(i) for i in range(10)]
+    val2 = ["{:02X}".format(10 * i) for i in range(10)]
+    val3 = [["" for c in range(10)] for r in range(10)]
+
+    fig, ax = plt.subplots()
+    ax.set_axis_off()
+    ax.table(
+        cellText=val3,
+        rowLabels=val2,
+        colLabels=val1,
+        rowColours=["palegreen"] * 10,
+        colColours=["palegreen"] * 10,
+        cellLoc='center',
+        loc='upper left')
+
+    ax.set_title('matplotlib.axes.Axes.table() function Example',
+                 fontweight="bold")
+
+    plt.show()
