9de54baa2d42e638060ade29db3e2e9a3c82a651
ed <ed.millard@gmail.com>
2026-05-09 18:47:53 -0600

    reservoir reality belongs in previous commit

--- graphs/reservoirs_reality.py
+++ graphs/reservoirs_reality.py
@@ -23,6 +23,9 @@ SOFTWARE.
 # from colorado.graph_inflow_outflow import InflowOutflowChart
 from colorado.graph_reservoirs import ReservoirChart
 from chart.chart_frame import ChartFrame, NotebookFrame
+import colorado.lb as lb
+from reservoirs.reservoir import Reservoir
+
 from reservoirs.imperial import Imperial
 # from reservoirs.roosevelt import Roosevelt
 from reservoirs.srp import SRP
@@ -36,12 +39,29 @@ from reservoirs.flaming_gorge import FlamingGorge
 from reservoirs.blue_mesa import BlueMesa
 from reservoirs.navajo import Navajo
 
+from reservoirs.green_mountain import GreenMountain
+from reservoirs.lake_granby import LakeGranby
+from reservoirs.mcphee import Mcphee
+from reservoirs.lake_nighthorse import LakeNighthorse
+from reservoirs.fontenelle import Fontenelle
+from reservoirs.lemon import Lemon
+from reservoirs.ruedi import Ruedi
+from reservoirs.taylor_park import TaylorPark
+from reservoirs.vallecito import Vallecito
+from reservoirs.starvation import Starvation
+from reservoirs.strawberry import Strawberry
+from reservoirs.grand_lake import GrandLake
+from reservoirs.shadow_mountain import ShadowMountain
+from reservoirs.heron import Heron
+
 class ReservoirsReality(ChartFrame):
     def __init__(self, notebook_frame: NotebookFrame):
         reports = ChartFrame.find_directories_with_file('data/USBR_24Month_Reports', 'Lake_Powell.csv')
 
         flaming_gorge = FlamingGorge()
         navajo = Navajo()
+
+        strawberry = Strawberry(upstream=[])
         blue_mesa = BlueMesa()
         lake_powell = LakePowell(upstream=[flaming_gorge, blue_mesa, navajo])
         lake_mead = LakeMead(upstream=[lake_powell])
@@ -53,20 +73,69 @@ class ReservoirsReality(ChartFrame):
         srp = SRP(upstream=[])
         aquifers = Aquifers(upstream=[])
 
-        reservoirs = [
+        self.reservoirs = [
             imperial, aquifers, srp, lake_pleasant, lake_havasu, lake_mohave,
-            lake_mead, lake_powell, flaming_gorge, navajo, blue_mesa
+            lake_mead, lake_powell, flaming_gorge, navajo, strawberry
         ]
 
-        super().__init__(notebook_frame, reservoirs=reservoirs, reports=reports, page_name='Reservoir Reality')
+        fontenelle = Fontenelle(upstream=[])
+        lake_granby = LakeGranby(upstream=[])
+        mcphee = Mcphee(upstream=[])
+        lake_nighthorse = LakeNighthorse(upstream=[])
+        taylor_park = TaylorPark(upstream=[])
+        vallecito = Vallecito(upstream=[])
+        green_mountain = GreenMountain(upstream=[])
+        starvation = Starvation(upstream=[])
+        ruedi = Ruedi(upstream=[])
+        # grand_lake = GrandLake(upstream=[])
+        # lemon = Lemon(upstream=[])
+        # shadow_mountain = ShadowMountain(upstream=[])
+        # heron = Heron(upstream=[])
+
+        self.ub_reservoirs = [
+            blue_mesa, lake_granby,
+            fontenelle,
+            vallecito, taylor_park,  mcphee, lake_nighthorse, green_mountain,  ruedi,
+            starvation,
+            # grand_lake, lemon, shadow_mountain, heron,
+        ]
+        reservoir_lists = [self.reservoirs, self.ub_reservoirs]
+        super().__init__(notebook_frame, reservoir_lists=reservoir_lists, reports=reports, page_name='Reservoir Reality')
+
 
     def load_charts(self):
         start = self.start_nav.current_date
         # current = self.current_nav.current_date
         end = self.end_nav.current_date
 
+        power_head_zones = [
+            ('#ffffff', 'Available Capacity'),
+            (Reservoir.high_power_pool_color, 'Normal Power Head'),
+            (Reservoir.low_power_pool_color, 'Low Power Head'),
+            (Reservoir.non_power_pool_color, 'Limited Access')
+        ]
+
+        aquifer_zones = [
+            (lb.TUCSON_COLOR, 'Tucson AMA'),
+            (lb.PINAL_COLOR, 'Pinal AMA'),
+            (lb.PHX_COLOR, 'Phoenix AMA')
+        ]
+
+        reserved_zones = [
+            (lb.AZ_COLOR, 'AZ'),
+            (lb.NV_COLOR, 'NV'),
+            (lb.CA_COLOR, 'CA')
+        ]
+
         reservoir_chart = ReservoirChart(
-            self.reservoirs, start_date=start, current_date=self.current_time_from_usbr, end_date=end
+            self.reservoirs, start_date=start, current_date=self.current_time_from_usbr, end_date=end, y_max=14.0,
+            power_head_zones=power_head_zones, reserved_names=reserved_zones, aquifer_zones=aquifer_zones
+        )
+        self.charts.append(reservoir_chart)
+
+        reservoir_chart = ReservoirChart(
+            self.ub_reservoirs, start_date=start, current_date=self.current_time_from_usbr, end_date=end, y_max=0.9,
+            percentage = 0.1
         )
         self.charts.append(reservoir_chart)
 
