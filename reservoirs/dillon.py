a895705ec224ac4f929b0c4a2981978e906d61aa
ed <ed.millard@gmail.com>
2026-05-10 09:58:16 -0600

    Add 3 CDSS reservoirs, elevation not working

---
+++ reservoirs/dillon.py
@@ -0,0 +1,69 @@
+"""
+Copyright (c) 2026 Ed Millard
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute copies of the Software, and
+to permit persons to whom the Software is furnished to do so, subject to the
+following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+from pathlib import Path
+from datetime import date
+from reservoirs.reservoir import Reservoir
+from typing import List, Optional
+from source import cdss
+
+
+class Dillon(Reservoir):
+    def __init__(self, upstream: Optional[List[Reservoir]] = None):
+        headers:List[str] = []
+        super().__init__('Dillon',headers,  catalog_id=0, upstream=upstream)
+        self.start_year = 1965
+        self.no_elevation_available = True
+
+        # Elevations
+        #
+        self.dead_pool_feet = 0
+        self.dead_pool_af = 0
+
+        self.full_feet = 9_017
+        self.full_af = 257_304
+
+        # Critical
+        self.power_head_target_feet = 0
+        self.power_head_target_af = 0
+
+        self.power_head_min_feet = 0
+        self.power_head_min_af = 0
+
+        self.turbine_intake_feet = 0
+        self.turbine_intake_af = 0
+        self.critical_elevations_feet = [("Safe Power Head", self.power_head_min_feet, self.power_head_min_af, Reservoir.non_power_pool_color),
+                                         ("Min Power Head", self.power_head_target_feet, self.power_head_target_af, Reservoir.low_power_pool_color)]
+
+    def load_data(self, report_path:Path, start_date: date, current_date: date, end_date: date):
+        # super().load_data(report_path, start_date, current_date, end_date)
+        time_series = cdss.telemetry_station_time_series(None, 'DILRESCO', 'STORAGE',
+                                                         water_year_info=self.water_year_info, alias='DILLON CAPACITY')
+        self.active_capacity_af = time_series[-1][1]
+
+        time_series = cdss.telemetry_station_time_series(None, 'DILRESCO', 'GAGE_HT',
+                                                         water_year_info=self.water_year_info, alias='DILLON ELEVATION')
+        if time_series is not None:
+            self.elevation_feet = time_series[-1][1]
+        # time_series = cdss.telemetry_station_time_series(logger, 'DILRESCO', 'DISCHRG',
+        #                                                 water_year_info=water_year_info, alias='DILLON DISCHARGE')
+        pass
+
---
+++ reservoirs/gross.py
@@ -0,0 +1,66 @@
+"""
+Copyright (c) 2026 Ed Millard
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute copies of the Software, and
+to permit persons to whom the Software is furnished to do so, subject to the
+following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+from pathlib import Path
+from datetime import date
+from reservoirs.reservoir import Reservoir
+from typing import List, Optional
+from source import cdss
+
+
+class Gross(Reservoir):
+    def __init__(self, upstream: Optional[List[Reservoir]] = None):
+        headers:List[str] = []
+        super().__init__('Gross',headers,  catalog_id=0, upstream=upstream)
+        self.start_year = 1955
+
+        # Elevations
+        #
+        self.dead_pool_feet = 0
+        self.dead_pool_af = 0
+
+        self.full_feet = 7_282
+        self.full_af = 41_811
+        # Denver Water is raising the dam by 131 feet (to ~471 feet total height).
+        # The enlarged reservoir will have ~119,000 acre-feet capacity at a new full pool elevation of 7,406 feet.
+        # Filling of the expanded reservoir is planned for 2027 (after construction completion).
+
+        # Critical
+        self.power_head_target_feet = 0
+        self.power_head_target_af = 0
+
+        self.power_head_min_feet = 0
+        self.power_head_min_af = 0
+
+        self.turbine_intake_feet = 0
+        self.turbine_intake_af = 0
+        self.critical_elevations_feet = [("Safe Power Head", self.power_head_min_feet, self.power_head_min_af, Reservoir.non_power_pool_color),
+                                         ("Min Power Head", self.power_head_target_feet, self.power_head_target_af, Reservoir.low_power_pool_color)]
+
+    def load_data(self, report_path:Path, start_date: date, current_date: date, end_date: date):
+        # super().load_data(report_path, start_date, current_date, end_date)
+        time_series = cdss.telemetry_station_time_series(None, 'GRORESCO', 'STORAGE',
+                                                         water_year_info=self.water_year_info, alias='GROSS CAPACITY')
+        self.active_capacity_af = time_series[-1][1]
+
+        # time_series = cdss.telemetry_station_time_series(logger, 'GRORESCO', 'DISCHRG',
+        #                                                 water_year_info=water_year_info, alias='GROSS DISCHARGE')
+
