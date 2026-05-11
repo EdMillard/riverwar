a43885966834854b0ae04d452a08bbb0e196fafc
ed <ed.millard@gmail.com>
2026-05-11 17:07:28 -0600

    WY Big Sandy reservoir

---
+++ reservoirs/big_sandy.py
@@ -0,0 +1,52 @@
+"""
+Copyright (c) 2026 Ed Millard
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute copies of the Software, and
+to permit persons to whom the Software is furnished to do so, subject to the
+following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+from reservoirs.reservoir import Reservoir
+from typing import List, Optional
+
+class BigSandy(Reservoir):
+    def __init__(self, upstream: Optional[List[Reservoir]] = None):
+        headers:List[str] = []
+        super().__init__('Big Sandy',headers,  catalog_id=2247, upstream=upstream)
+        self.start_year = 1953
+
+        # Elevations
+        #
+        self.dead_pool_feet = 0
+        self.dead_pool_af = 0
+
+        self.full_feet = 6_757.5
+        self.full_af = 38_600
+        # An enlargement project (raising the spillway by 5 feet) is planned or in progress to increase
+        # capacity by ~12,900–13,700 acre-feet (to ~51,500–52,300 acre-feet total)
+
+        # Critical
+        self.power_head_target_feet = 0
+        self.power_head_target_af = 0
+
+        self.power_head_min_feet = 6_855
+        self.power_head_min_af = 152_000
+
+
+        self.turbine_intake_feet = 0
+        self.turbine_intake_af = 0
+        self.critical_elevations_feet = [("Safe Power Head", self.power_head_min_feet, self.power_head_min_af, Reservoir.non_power_pool_color),
+                                         ("Min Power Head", self.power_head_target_feet, self.power_head_target_af, Reservoir.low_power_pool_color)]
